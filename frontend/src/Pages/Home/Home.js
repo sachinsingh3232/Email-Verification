4f2590313f879fb56efbfdd33e0b53634c3bdff7
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2024-04-16 23:05:15 +0530

    Update Home.js

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -11,8 +11,16 @@ const Home = () => {
     };
     setUser(JSON.parse(localStorage.getItem("user")))
   }, [])
+  const logoutHandler = () => {
+    localStorage.removeItem("user")
+    navigate('/login');
+  }
   return (
     <div className='home'>
+      <div className='nav'>
+        <img src='https://highwaydelite.com/assets/logo2-a2f23723.webp' alt='' className='img'/>
+        <span onClick={logoutHandler} className='logout'>logout</span>
+      </div>
       <h1>Welcome {user.fName + " " + user.lName} </h1>
     </div>
   )
